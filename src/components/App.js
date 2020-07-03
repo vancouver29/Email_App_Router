951c80e0e0e6f7ba4641cda02523f495928dd143
vancouver29 <vancouver2990@gmail.com>
2020-07-03 19:39:34 +0200

    Automatin Project Setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div id="app-container">
+        <h1>My React App Template</h1>
+        <p>Welcome to my React App Template </p>
+      </div>
+    );
+  }
+}
